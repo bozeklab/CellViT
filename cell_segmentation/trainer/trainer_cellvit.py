b89269b43636cf20b835279ab5f956607a5f1cd1
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2023-10-11 13:18:46 -0700

    fix config

--- cell_segmentation/trainer/trainer_cellvit.py
+++ cell_segmentation/trainer/trainer_cellvit.py
@@ -164,7 +164,7 @@ class CellViTTrainer(BaseTrainer):
         train_loop = tqdm.tqdm(enumerate(train_dataloader), total=len(train_dataloader))
 
         for batch_idx, batch in train_loop:
-            u_img = train_u_dataloader_iter.next()
+            u_img = next(train_u_dataloader_iter)
             return_example_images = batch_idx == select_example_image
             batch = batch[0], u_img, batch[1], batch[2]
             batch_metrics, example_img = self.train_step(
